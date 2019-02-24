dbab6036846047bc5f50bdb393029e4928e341a9
Gabor Csepregi <csepregigabor02@gmail.com>
2019-02-24 06:47:59 +0100

    first es6 homework

---
+++ HomeWork1/song.js
@@ -0,0 +1,32 @@
+/* 
+Keep Up With ES6
+First Assignment
+Write your favourite song
+*/
+
+var artist = "Kavinsky"
+var cooperateArtist = "Lovefoxxx"
+var genre = "Synthwave"
+var published = 2011
+var Album = "Drive"
+var good = true
+var durationInSeconds = 258
+
+console.log(artist);
+console.log(cooperateArtist);
+console.log(published);
+console.log(good);
+console.log(durationInSeconds);
+
+var song = {
+    artist : "Kavinsky",
+    cooperateArtist : "Lovefoxxx",
+    genre : "Synthwave",
+    published : 2011,
+    Album : "Drive",
+    good : true,
+    durationInSeconds : 258,
+}
+
+console.log(song.Album);
+console.log("Good? " + song.good);
